130c615b9d24d369aa4ddd0e5442b013adc1b622
Sharath Mohan <sharathadm97@gmail.com>
2020-11-15 13:13:37 +0530

    Initial Commit

--- imgupload/urls.py
+++ imgupload/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home,name='home'),
-    path('imageprocess',views.imageprocess,name='imageprocess')
+    path('/imageprocess',views.imageprocess,name='imageprocess')
 
 ]
