d32448eb156f8106eb3221a8b5235edd1934d8be
Sharath Mohan <sharathadm97@gmail.com>
2023-04-27 15:13:54 -0400

    Updated the URLs and created new paths

--- DogBreed/urls.py
+++ DogBreed/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('imgupload',include('imgupload.urls')),
+    path('',include('imgupload.urls')),
     path('admin/', admin.site.urls),
 ]
